836376a7ed5fe76ce569f8667d6f817c72b87374
Tucker <zachary.tucker1@pcc.edu>
2019-10-23 14:42:45 -0700

    added a print date to the method

--- src/edu/pcc/cis/HelloWorld/HelloWorld.java
+++ src/edu/pcc/cis/HelloWorld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.HelloWorld;
 
+import java.util.Date;
+
 /**
  *  Class to print out hello message
  */
@@ -8,6 +10,7 @@ public class HelloWorld
     public static void main(String [] args)
     {
         System.out.println ("Hello World");
+        System.out.println(new Date());
 
     }
 
